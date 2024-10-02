1bb6006faf7133407382405c79dcf1f205ab1eaa
Giuseppe Partipilo <gpartipilo726@gmail.com>
2024-10-02 15:21:17 -0400

    finished adding game logic and ensured game winner is found. Added reset button as well.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 
 //manages the state of the gameboard
 const Gameboard = function () {
-
+    let turn = 1;
     const gameboard = [
         [1,2,3],
         [4,5,1],
@@ -16,23 +16,22 @@ const Gameboard = function () {
         });
     };
 
-    const updateGameBoard = (row, column, player) =>{
-        console.log("inside Update board. Row is" + row + "Col is" + column);
-        row -= 1;
-        column -= 1;
+    const updateGameBoard = (selectedSquare, player) =>{
+        const row = selectedSquare.getAttribute("gridrow");
+        const column = selectedSquare.getAttribute("gridcol");
 
-        if (gameboard[row][column] === 'x' || gameboard[row][column] === 'o') {
+        if (selectedSquare.innerHTML === 'X' || selectedSquare.innerHTML === 'O') {
             console.log("That Spot is already taken. Please Try again.");
-            updateGameBoard(player.getPlayerRowChoice(), player.getPlayerColChoice(), player);
+            return false;
         } else { 
             gameboard[row][column] = player.getPlayerSymbol();
+            selectedSquare.innerHTML = player.getPlayerSymbol();
         }
     
     };
 
-    const checkWinner = (player) => {
+    const checkWinner = (player, messageDiv) => {
         let gameOver = false;
-
         console.log("inside the checkWinner function. Player symbol is " + player.getPlayerSymbol())
 
         if( 
@@ -53,19 +52,34 @@ const Gameboard = function () {
             //bottom horizontal
             (gameboard[2][0] === gameboard[2][1] && gameboard[2][1] === gameboard[2][2] && gameboard[2][0] === player.getPlayerSymbol())
         ){
-            console.log(player.getPlayer() + " is the Winner! GAME OVER!");
+            messageDiv.innerHTML = `${player.getPlayerName()} is the Winner!`;
             return gameOver = true;
         } else {
             return gameOver;
         }
     };
 
+    const getTurn = () => {
+        const messageDesc = document.querySelector("#message");
+
+        turn++;
+        if (turn % 2 === 0){
+            messageDesc.innerHTML =`${playerOne.getPlayerName()}'s Turn!`;
+            return 2;
+        } else {
+            messageDesc.innerHTML =`${playerTwo.getPlayerName()}'s Turn!`;
+            return 1;
+        };
+
+
+    }
+
     const resetGameboard = () => {
 
     };
 
 
-    return {getGameboard, updateGameBoard, checkWinner, resetGameboard}
+    return {getGameboard, updateGameBoard, checkWinner, getTurn, resetGameboard}
 
 };
 
@@ -74,31 +88,20 @@ const player = function () {
     let playerSymbol;
     let playerName;
 
-    const createPlayer = (name) => {
+    const createPlayer = (name, symbol) => {
         playerName = name;
-        const symbol = prompt(`Hi ${name}, do you want to be X's or O's?`);
         setPlayerSymbol(symbol);
-        console.log("inside the createPlayer function. Player symbol is " + getPlayerSymbol())
-    }
-
-    const getPlayerRowChoice = () =>{
-        let val = prompt("Enter a row number:");
-        console.log("row val is " + val)
-        return val
-
-    }
-
-    const getPlayerColChoice = () =>{
-        let val = prompt("Enter a column number:");
-        console.log("col val is " + val)
-        return val
     }
     
     const setPlayerSymbol = (symbol) => {
         playerSymbol = symbol
         }
 
-    const getPlayer = () =>{
+    const setPlayerChoice = (choice) =>{
+
+    }
+
+    const getPlayerName = () =>{
         return playerName;           
     }
 
@@ -106,52 +109,149 @@ const player = function () {
         return playerSymbol;
     }
     
-    return {createPlayer, getPlayer, getPlayerRowChoice, getPlayerColChoice, getPlayerSymbol};
+    return {createPlayer, getPlayerName, getPlayerSymbol, setPlayerChoice};
 
 };
 
 
 //controls the flow of the game
 const gameController = function (){
+    const playerOneName = document.querySelector("#playerOne");
+    const playerOneSymbol = document.querySelector("#playerOneSymbol");
+    const playerTwoName = document.querySelector("#playerTwo");
+    const playerTwoSymbol = document.querySelector("#playerTwoSymbol");
+    const startBtn = document.querySelector("#startgame");
+
+const updateDropdown = () => {
+
+    // Get the selected value from the first dropdown
+    const playerOneSel = playerOneSymbol.value;
+    const playerTwoSel = playerTwoSymbol.value;
 
 
-const startGame = () => {
-    let gameboard = Gameboard();
-    let endGame = false;
+    // Enable all options in the second dropdown first
+    Array.from(playerTwoSymbol.options).forEach(option => {
+        option.disabled = false;
+    });
 
-    //ask for player name
-    let playerOne = player();
-    playerOne.createPlayer("Jim");
-    
-    let playerTwo = player();
-    playerTwo.createPlayer("Ashley");
+    Array.from(playerOneSymbol.options).forEach(option => {
+        option.disabled = false;
+    });
 
-    //start game
-    while (!endGame){
-        console.log("----- || Player One's Turn  || -----");
-        gameboard.updateGameBoard(playerOne.getPlayerRowChoice(), playerOne.getPlayerColChoice(), playerOne);
-        gameboard.getGameboard();
-        if(gameboard.checkWinner(playerOne)){
-            endGame = true;
-            return;
-        };
-
-        console.log("----- || Player Two's Turn  || -----");
-        gameboard.updateGameBoard(playerTwo.getPlayerRowChoice(), playerTwo.getPlayerColChoice(), playerTwo);
-        gameboard.getGameboard();
-        if(gameboard.checkWinner(playerTwo)){
-            endGame = true;
-            return;
-        };
+    // Disable the selected option in the second dropdown if it's not empty
+    if (playerOneSel) {
+        playerTwoSymbol.querySelector(`option[value="${playerOneSel}"]`).disabled = true;
+    } else if (playerTwoSel) {
+        playerOneSymbol.querySelector(`option[value="${playerTwoSel}"]`).disabled = true;
     }
+
+};    
+
+const getPlayerDetails = () =>{
+
+
+    startBtn.addEventListener("click", (e) =>{
+        e.preventDefault();
+        console.log("Player One Name: " + playerOneName.value);
+        console.log("Player One Symbol: " + playerOneSymbol.value);
+        console.log("Player Two Name: " + playerTwoName.value);
+        console.log("Player Two Symbol: " + playerTwoSymbol.value);
+        
+        playerOne = player();
+        playerOne.createPlayer(playerOneName.value, playerOneSymbol.value);
+
+        playerTwo = player();
+        playerTwo.createPlayer(playerTwoName.value, playerTwoSymbol.value);
+
+
+        startGame(playerOne, playerTwo);
+
+    });
 
 }
 
-return {startGame}
+const startGame = (playerOne, playerTwo) => {
+    let gameboard = Gameboard();
+    let endGame = false;
+    const messageDesc = document.querySelector("#message");
+    const gameboardDiv = document.querySelector(".gameboard");
+    let turn = gameboard.getTurn();
+
+    messageDesc.innerHTML =`${playerOne.getPlayerName()}'s Turn!`;
+
+    gameboardDiv.addEventListener("click", (e) => {
+        selectedSquare = e.target.closest(".grid-body");
+        let turn = gameboard.getTurn();
+
+
+        if(turn === 1){
+            gameboard.updateGameBoard(selectedSquare, playerOne);
+            if(gameboard.checkWinner(playerOne, messageDesc)){
+                gameboardDiv.removeEventListener("click", ()=>{
+                    console.log("event removed from gameboard")
+                });
+            };
+
+            console.log("inside player one if statement");
+        } else {
+            gameboard.updateGameBoard(selectedSquare, playerTwo);
+            if(gameboard.checkWinner(playerTwo, messageDesc)){
+                gameboardDiv.removeEventListener("click", ()=>{
+                    console.log("event removed from gameboard")
+                });
+
+            };
+        }
+        console.log("still inside evetn listener")
+
+        
+     
+    });
+
+    //messageDesc.innerHTML =`${playerTwo.getPlayerName()}'s Turn!`;
+    // gameboardDiv.addEventListener("click", (e) => {
+    //     selectedSquare = e.target.closest(".grid-body");
+    //     gameboard.updateGameBoard(selectedSquare, playerTwo);
+    //     console.log(selectedSquare);
+    //     return;
+
+    // });
+
+    console.log("moving on after gameboard clicked");
+
+    //start game
+    //const PlayerOneChoice = playerOne.getPlayerChoice();
+    //gameboard.updateGameBoard(PlayerOneChoice, playerOne);
+    //gameboard.checkWinner(playerOne);
+
+    //const PlayerTwoChoice = playerTwo.getPlayerChoice();
+    //gameboard.updateGameBoard(PlayerTwoChoice, playerTwo);
+    //gameboard.checkWinner(playerTwo);
+
+
+    //gameboard.updateGameBoard(playerOne.getPlayerRowChoice(), playerOne.getPlayerColChoice(), playerOne);
+    // gameboard.getGameboard();
+    // if(gameboard.checkWinner(playerOne)){
+    //     endGame = true;
+    //     return;
+    // };
+
+    // messageDesc.innerHTML =`${playerTwo.getPlayerName()}'s Turn`
+    // gameboard.updateGameBoard(playerTwo.getPlayerRowChoice(), playerTwo.getPlayerColChoice(), playerTwo);
+    // gameboard.getGameboard();
+    // if(gameboard.checkWinner(playerTwo)){
+    //     endGame = true;
+    //     return;
+    // };
+
+
+}
+
+return {startGame, getPlayerDetails, updateDropdown}
 
 };
 
 const game = gameController();
-game.startGame();
+game.getPlayerDetails();
 console.log("end of program")
 
